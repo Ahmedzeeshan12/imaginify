7732556b8935fd8ee4a573da2e15bc100aa88517
Ahmedzeeshan12 <zeeshanhamed1152@gmail.com>
2024-05-16 18:27:40 +0500

    project

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>UpdateTransformationPage</div>
+  )
+}
+
+export default UpdateTransformationPage
+
+
+// clerk_trace_id": "cf896555d04dea99f7b9c0296bc7a078"
---
+++ lib/database/models/transaction.model.ts
@@ -0,0 +1,34 @@
+
+import { Schema, model, models,  } from "mongoose";
+
+
+const transactionSchema = new Schema({
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    stripeId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    plan:{
+        type:String
+    },
+    credits:{
+        type:Number
+    },
+    buyer:{
+      type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+});
+
+const transaction = models?.transaction || model("Transaction",transactionSchema);
+
+export default transaction
